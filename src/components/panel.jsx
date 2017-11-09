e9d8dbd7326d16944961449cafe1dac6565b8f76
adammertel <mertel.adam@gmail.com>
2017-11-09 12:17:44 +0100

    panel styling

--- src/components/panel.jsx
+++ src/components/panel.jsx
@@ -6,28 +6,21 @@ import ShapeFilter from './shapefilter';
 
 @observer
 class Panel extends React.Component {
-  style() {
-    return {
-      height: 300,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      position: 'fixed'
-    };
-  }
-
   render() {
     return (
-      <div className="panel-wrapper" style={this.style()}>
-        <section className="hero is-primary" style={{ height: 300 }}>
-          <div className="hero-head" style={{ paddingTop: 10 }}>
+      <div className="panel-wrapper">
+        <section className="hero is-primary">
+          <div className="hero-body">
             <div className="container">
-              <h1 className="title">BAPTISTERIA</h1>
-              <h2 className="subtitle">Map application</h2>
-            </div>
-            <div className="hero-body">
-              <TimeSlider />
-              <ShapeFilter />
+              <h1 className="title">BAPTISTERIA Interactive Map</h1>
+              <h3 className="subtitle">Show only buildings built before:</h3>
+              <div className="panel-control">
+                <TimeSlider />
+              </div>
+              <h3 className="subtitle">Filter buildings based on the shape:</h3>
+              <div className="panel-control">
+                <ShapeFilter />
+              </div>
             </div>
           </div>
         </section>
