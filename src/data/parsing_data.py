8fec4bbe9eece85ea735dab241ecb450e5f20312
adammertel <mertel.adam@gmail.com>
2017-11-17 12:44:54 +0100

    new baptisteries attributes

--- src/data/parsing_data.py
+++ src/data/parsing_data.py
@@ -8,12 +8,14 @@ for ri, row in enumerate(baptisterias):
     if row['Certainty'] == 'certain or very probable':
         try:
             props = {
-                "name": row['Localisation'],
+                "name": row['Place_name_modern'],
                 "date": int(row['Date_average'], 10),
+                "date_after": int(row['Built_after_or_in'], 10),
+                "date_before": int(row['Built_before_or_in'], 10),
                 "shape": row['Building_shape_1'].strip(),
-                "specification": row['Place_specification'],
                 "piscina_shape": row['Piscina_shape_1'],
-                "piscina_depth": row['Piscina_depth_cm']
+                "piscina_depth": row['Piscina_depth_cm'],
+                "localisation_certainty": row['Localisation_certainty']
             }
             #print(float(row['X_coordinates']))
             point = Point([float(row['X_coordinates']), float(row['Y_coordinates'])])
