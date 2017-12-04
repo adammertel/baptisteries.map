94c051951d3827f010ebd41f4807f0529c76ae9b
adammertel <mertel.adam@gmail.com>
2017-12-04 17:12:14 +0100

    new info text

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -21,30 +21,33 @@ class Panel extends React.Component {
           <section className="modal-card-body">
             <div className="content">
               <p className="has-text-primary title is-4">
-                Christian baptisteries - interactive map
+                Christian baptisteries: interactive map
               </p>
               <p>
                 This interactive map visualizes a database of Christian
-                baptisteries built between the 3rd and the 12th century. The
+                baptisteries built between the 3rd and 12th centuries. The
                 database is a digital adaptation and formalization of the most
                 complete (as of 2017) catalogue of baptisteries by Sebastian
                 Ristow (<i>Frühchristliche Baptisterien</i>, Münster:
                 Aschendorffsche Verlagsbuchhandlung, 1998) and was compiled by
-                Hana Hořínková in the framework of her B.A. thesis at Masaryk
-                University’s Department for the Study of Religions (2017),
-                supervised by
+                Hana Hořínková in the framework of her{' '}
+                <a href="http://is.muni.cz/th/439223/ff_b/?lang=en">
+                  B.A. thesis
+                </a>{' '}
+                at Masaryk University’s Department for the Study of Religions
+                (2017), supervised by
                 <a href="http://www.david-zbiral.cz/"> David Zbíral</a>. The map
                 was conceived and created by Adam Mertel (Masaryk University,
                 Department of Geography).
               </p>
               <p>
                 Of all 1067 buildings in Ristow’s catalogue, this visualization
-                takes into account only the buildings whose existence and
-                function as baptisteries is considered certain or very probable
-                in Ristow’s catalogue and at the same time they were
-                successfully localised (778 records, which amounts to 98,1 % of
-                certain or very probable baptisteries catalogued by Ristow). The
-                localisation has three degrees of certainty:
+                takes into account only those whose existence and function as
+                baptisteries is considered certain or very probable and which,
+                at the same time, were successfully localised (778 records,
+                which amounts to 98.1 % of all certain or very probable
+                baptisteries catalogued by Ristow). The localisation has three
+                degrees of certainty:
               </p>
               <ul>
                 <li>
@@ -52,7 +55,7 @@ class Panel extends React.Component {
                   particular settlement
                 </li>
                 <li>
-                  <strong>approximate</strong> when the particular place or
+                  <strong>approximate</strong> when the particular place
                   settlement was not found but a nearby settlement was
                 </li>
                 <li>
@@ -61,11 +64,11 @@ class Panel extends React.Component {
                 </li>
               </ul>
               <p>
-                The degree of localisation certainty of a particular baptistery
-                can be displayed under the individual record by clicking on the
-                icon of that particular baptistery, together with some more
-                data: modern place name, building shape,{' '}
-                <i>terminus post quem</i>
+                The degree of certainty of the localisation of a particular
+                baptistery can be displayed under the individual record by
+                clicking on the icon of that particular baptistery. This appears
+                with some further data: modern place name, building shape,{' '}
+                <i>terminus post quem </i>
                 (“built after or in”), <i>terminus ante quem</i> (“built before
                 or in”), the mean value between the <i>terminus post quem</i>{' '}
                 and <i>ante quem</i>, piscina shape, and piscina depth.
@@ -74,11 +77,12 @@ class Panel extends React.Component {
                 When zoomed out, the map shows a hexagonal grid. The shade of an
                 individual hexagon denotes the median of the mean values between
                 the <i>terminus post quem</i> and <i>ante quem</i> of all
-                baptisteries in that hexagon; darker the shade, earlier the
-                date. When zoomed in, the map shows icons of individual
-                baptisteries or several aggregated baptisteries (in that case,
-                their number is displayed). The shape in the icon translates the
-                shape of the building (see the Legend).
+                baptisteries in that hexagon; the darker the shade, the earlier
+                the date. When zoomed in, the map shows icons of individual
+                baptisteries or several aggregated baptisteries (in the latter
+                case, their number is displayed). The shape in the icon
+                translates to the shape of the building (see the legend in the
+                left panel).
               </p>
               <p>The records displayed can be filtered in two ways:</p>
               <ul>
@@ -88,18 +92,24 @@ class Panel extends React.Component {
                 </li>
                 <li>
                   the timeline can be limited in order to display only a part of
-                  the whole dataset, whose date (based on the mean value between
-                  the <i>terminus post quem</i> and <i>ante quem</i>) falls
-                  between 240 and the date selected by the user. Dragging the
-                  slider or turning the mouse wheel over it allows to move
-                  through time and see new baptisteries appear;
+                  the whole dataset whose dates(based on the mean values between
+                  the <i>terminus post quem</i> and <i>ante quem</i>) fall
+                  between the dates selected by the user. Dragging the slider,
+                  or turning the mouse wheel over it allows the user to move
+                  through time and see new baptisteries appear.
                 </li>
               </ul>
               <p>
-                if the map is zoomed out, the shade of hexagons changes as well,
-                showing the calculated change of the median of the mean dates of
-                baptisteries in that hexagon.
+                When zooming out to the level of hexagons and moving the slider,
+                the shade of the hexagons also changes, showing the change in
+                the calculated median of the mean dates of baptisteries in that
+                hexagon.
               </p>
+              <p>
+                The base map can be selected under the icon in the upper right
+                corner.
+              </p>
+
               <hr />
               <p className="has-text-primary title is-5">Map Legend</p>
               <GridLegend />
