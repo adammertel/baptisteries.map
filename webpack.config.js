0d8efc1c7e5005c05e2fb74842c4eca441941d36
adammmertel <mertel.adam@gmail.com>
2020-02-25 21:42:19 +0100

    BUILD: webpack reinstalled

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,65 @@
-const webpack = require('webpack');
-const path = require('path');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
   devServer: {
     historyApiFallback: true,
     hot: true,
     inline: true,
-    contentBase: './src',
+    contentBase: "./src",
     port: 8080
   },
   entry: {
-    main: './src/index',
-    static_shape: './src/static_shape/index',
-    static_piscina: './src/static_piscina/index',
-    static_small: './src/static_small/index'
+    main: "./src/index",
+    static_shape: "./src/static_shape/index",
+    static_piscina: "./src/static_piscina/index",
+    static_small: "./src/static_small/index"
   },
   output: {
-    filename: '[name].bundle.js'
+    filename: "[name].bundle.js"
   },
   module: {
     rules: [
       {
         test: /\.js[x]?$/,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, "src"),
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: ['react']
+          presets: ["react"]
         }
       },
       {
         test: /\.scss$/,
         use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'resolve-url-loader' },
-          { loader: 'sass-loader' }
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "resolve-url-loader" },
+          { loader: "sass-loader" }
         ]
       },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
+        loader: "url-loader?limit=100000"
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
+        loader: "file-loader"
       }
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('development')
+      "process.env": {
+        NODE_ENV: JSON.stringify("development")
       }
     }),
     new webpack.HotModuleReplacementPlugin()
