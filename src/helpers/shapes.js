5454bd995f888914dcc38b9782aa0a0a98965a67
adammmertel <mertel.adam@gmail.com>
2020-04-21 17:09:44 +0200

    DATA: Update config files

--- src/helpers/shapes.js
+++ src/helpers/shapes.js
@@ -1,132 +1,122 @@
-import React from 'react'
-
 var Shapes = {
   colors: [
-    '#e41a1c',
-    '#377eb8',
-    '#4daf4a',
-    '#984ea3',
-    '#ff7f00',
-    '#ffff33',
-    '#a65628',
-    '#f781bf',
-    '#999999',
-    'black'
+    "#e41a1c",
+    "#377eb8",
+    "#4daf4a",
+    "#984ea3",
+    "#ff7f00",
+    "#ffff33",
+    "#a65628",
+    "#f781bf",
+    "#999999",
+    "black",
   ],
   shapesDictionary: [
     {
-      label: 'rectangle',
-      values: ['rectangle'],
-      icon: '9646',
-      color: 0
+      label: "rectangle",
+      values: ["rectangle"],
+      icon: "9646",
+      color: 0,
     },
     {
-      label: 'square',
-      values: ['square'],
-      icon: '9632',
-      color: 1
+      label: "square",
+      values: ["square"],
+      icon: "9632",
+      color: 1,
     },
     {
-      label: 'circle',
+      label: "circle",
+      values: ["round", "circle", "semicircle", "three-quarter round", "oval"],
+      icon: "9679",
+      color: 2,
+    },
+    {
+      label: "central building",
+      values: ["central building"],
+      icon: "9711",
+      color: 3,
+    },
+    {
+      label: "inside the church",
+      values: ["inside the church"],
+      icon: "10752",
+      color: 4,
+    },
+    {
+      label: "cross",
+      values: ["cross"],
+      icon: "10010",
+      color: 5,
+    },
+    {
+      label: "foil",
+      values: ["trefoil", "quatrefoil", "octofoil"],
+      icon: "9827",
+      color: 6,
+    },
+    {
+      label: "polygon",
       values: [
-        'round',
-        'circle',
-        'semicircle',
-        'three-quarter round',
-        'oval'
+        "triangle",
+        "hexagon",
+        "octogon",
+        "dodekagon",
+        "dekagon",
+        "heptagon",
       ],
-      icon: '9679',
-      color: 2
+      icon: "11039",
+      color: 7,
     },
     {
-      label: 'central building',
-      values: ['central building'],
-      icon: '9711',
-      color: 3
-    },
-    {
-      label: 'inside the church',
-      values: ['inside the church'],
-      icon: '10752',
-      color: 4
-    },
-    {
-      label: 'cross',
-      values: ['cross'],
-      icon: '10010',
-      color: 5
-    },
-    {
-      label: 'foil',
-      values: ['trefoil', 'quatrefoil', 'octofoil'],
-      icon: '9827',
-      color: 6
-    },
-    {
-      label: 'polygon',
+      label: "other",
       values: [
-        'triangle',
-        'hexagon',
-        'octogon',
-        'dodekagon',
-        'dekagon',
-        'heptagon'
+        "trapezoid",
+        "irregular",
+        "other",
+        "polygon",
+        "horseshoe",
+        "rosette",
       ],
-      icon: '11039',
-      color: 7
+      icon: "9676",
+      color: 9,
     },
     {
-      label: 'other',
-      values: [
-        'trapezoid',
-        'irregular',
-        'other',
-        'polygon',
-        'horseshoe',
-        'rosette'
-      ],
-      icon: '9676',
-      color: 9
+      label: "unknown",
+      values: ["", "unknown"],
+      icon: "?",
+      color: 8,
     },
-    {
-      label: 'unknown',
-      values: ['', 'unknown'],
-      icon: '?',
-      color: 8
-    }
   ],
   defaultShape: () => {
-    return Shapes.getShape('other')
+    return Shapes.getShape("other");
   },
   getIcon: (shapeValue, label = false) => {
-    const shape = Shapes.getShape(shapeValue, label)
-    return shape ? String.fromCharCode(shape.icon) : false
+    const shape = Shapes.getShape(shapeValue, label);
+    return shape ? String.fromCharCode(shape.icon) : false;
   },
   getShape: (shapeValue, label = false) => {
-    const shape = Shapes.shapesDictionary.find(s => {
-      return label
-        ? s.label === shapeValue
-        : s.values.includes(shapeValue)
-    })
-    return shape || Shapes.defaultShape()
+    const shape = Shapes.shapesDictionary.find((s) => {
+      return label ? s.label === shapeValue : s.values.includes(shapeValue);
+    });
+    return shape || Shapes.defaultShape();
   },
-  getLabel: shapeValue => {
-    const shape = Shapes.getShape(shapeValue)
-    return shape ? shape.label : false
+  getLabel: (shapeValue) => {
+    const shape = Shapes.getShape(shapeValue);
+    return shape ? shape.label : false;
   },
   getColor: (shapeValue, label = false) => {
-    const shape = Shapes.getShape(shapeValue, label)
-    return shape ? Shapes.colors[shape.color] : false
+    const shape = Shapes.getShape(shapeValue, label);
+    return shape ? Shapes.colors[shape.color] : false;
   },
   getIndex: (shapeValue, label = false) => {
-    const shape = Shapes.getShape(shapeValue, label)
-    return Shapes.shapesDictionary.indexOf(shape)
+    const shape = Shapes.getShape(shapeValue, label);
+    return Shapes.shapesDictionary.indexOf(shape);
   },
   sortByIndex: (a, b) => {
-    const idA = Shapes.getIndex(a.label, true)
-    const idB = Shapes.getIndex(b.label, true)
-    return idA > idB ? 1 : -1
-  }
-}
+    const idA = Shapes.getIndex(a.label, true);
+    const idB = Shapes.getIndex(b.label, true);
+    return idA > idB ? 1 : -1;
+  },
+};
 
-module.exports = Shapes
+module.exports = Shapes;
