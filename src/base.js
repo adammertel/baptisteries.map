86f159531f10bc7dd27282d214c670288b5fa600
adammmertel <mertel.adam@gmail.com>
2020-04-21 17:11:52 +0200

    REFACTOR: Unify get data method

--- src/base.js
+++ src/base.js
@@ -1,42 +1,15 @@
 var Base = {
-  doRequestSync(url) {
-    let xhr = new XMLHttpRequest();
-    xhr.withCredentials = false;
-    xhr.open('GET', url, false);
-    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-    xhr.send();
-
-    if (xhr.status === 200) {
-      return xhr.responseText;
-    } else {
-      return {};
-    }
-  },
-
-  doRequestAsync(url) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    //xhr.withCredentials = true;
-    xhr.setRequestHeader('Access-Control-Allow-Origin', 'http://adam:8080');
-
-    xhr.setRequestHeader('X-PINGOTHER', 'pingpong');
-    xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET');
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-
-    xhr.onload = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          return xhr.responseText;
-        } else {
-          //console.log(xhr.statusText);
-          return {};
-        }
-      } else {
-        return {};
+  getJSONData(path, next) {
+    let xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open("GET", path, true);
+    xobj.onreadystatechange = () => {
+      if (xobj.readyState === 4 && xobj.status === 200) {
+        console.log(xobj.responseText);
+        next(JSON.parse(xobj.responseText));
       }
     };
-
-    xhr.send();
+    xobj.send(null);
   },
 
   validGeo(feat) {
@@ -51,7 +24,7 @@ var Base = {
 
   compareShapes(sh1, sh2) {
     let same = true;
-    Object.keys(sh1).map(sk1 => {
+    Object.keys(sh1).map((sk1) => {
       if (sh2[sk1] !== sh1[sk1]) {
         same = false;
       }
@@ -66,38 +39,6 @@ var Base = {
       window.getSelection().removeAllRanges();
     }
   },
-
-  getData(path, next) {
-    const req = new XMLHttpRequest();
-    req.open('GET', path, true);
-    req.withCredentials = false;
-    req.send();
-    const success = out => {
-      console.log(out);
-      next(JSON.parse(out.responseText));
-    };
-    const error = status => {
-      console.log(status);
-      next(false);
-    };
-
-    req.onreadystatechange = function() {
-      if (req.readyState == 4) {
-        return req.status === 200 ? success(req) : error(req.status);
-      }
-    };
-  },
-
-  requestConfigFile(configName, sync, next = false) {
-    const configPath = './' + configName;
-    if (sync) {
-      return JSON.parse(this.doRequestSync(configPath));
-    } else {
-      this.doRequest(configPath, response => {
-        next(JSON.parse(response));
-      });
-    }
-  }
 };
 
 module.exports = Base;
